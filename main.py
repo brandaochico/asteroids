6400e07941d3c3c631e7e26ee8bba3c162e479a7
brandaochico <brandao.chico24@gmail.com>
2026-02-05 21:37:52 -0300

    player movement and rotation / sprites groups

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -25,7 +30,12 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
